5092f8d0036f638a488642c987ebd858297bd8f0
Roma Bondar <bond2000r@mail.ru>
2020-03-07 02:52:52 +0300

    lattice class added

--- ising.cpp
+++ ising.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
+
+class lattice {
+protected:
+    int N;	//number of spins
+    int *L;	//lattice as array
+
+public:
+	lattice () : N (0), L (nullptr) {cout << "lattice ()\n";}
+	lattice (int N)	: N (N), L (new int [N])	{}
+	/*void fill_random ()	{
+		//if (
+	}*/
+	virtual void show ()	{}
+	virtual ~lattice ()	{
+		delete L;
+		cout << "~lattice ()\n";
+	}
+};
+
+class square_lattice : public lattice	{
+public:
+	square_lattice ()	{cout << "sq_lattice ()\n";}
+	~square_lattice ()	{cout << "~sq_lattice ()\n";}
+};
+
 int main ()  {
-    printf ("Hello Sanya");
-    printf ("Hell Sanya!");
+	lattice *l = new square_lattice ();
+	delete l;
     return 0;
 }
