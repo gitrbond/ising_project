429d8ca218a82ea989d7430cfd917e09707ec872
Stasyan <stanislav.pyatkin@mail.ru>
2020-03-07 19:27:49 +0300

    Написал fill_random

--- ising.cpp
+++ ising.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
@@ -10,16 +12,16 @@ protected:
 
 public:
 	lattice () : N (0), L (nullptr)	{cout << "lattice ()\n";}
-	lattice (int N)	: N (N), L (new int [N])	{cout << "lattice (" << N << ")\n"}
+	lattice (int N)	: N (N), L (new int [N])	{cout << "lattice (" << N << ")\n";}
 	
 	void fill_random ()	{
-		int i;
-		for (i = 0; i < N; i++)	{
-			//L[i] = random();
+		srand(time(0));
+		for (int i = 0; i < N; i++)	{
+			L[i] = 2 * (rand() % 2) - 1;
 		}
 	}
 
-	virtual void show ();
+	virtual void show () {}
 
 	virtual ~lattice ()	{
 		delete L;
