9d9a7ccdbf890698943954fcc4a885014cd052a8
Roma Bondar <bond2000r@mail.ru>
2020-03-14 04:25:48 +0300

    some trash cleaned

--- ising.cpp
+++ ising.cpp
@@ -6,16 +6,16 @@
 
 using namespace std;
 
-int rand_30bit();
+int big_rand();
 
 class lattice {
 protected:
 	int N; //number of spins
 	int *L; //spins in array
-	int neighbours; //nuber of neighbours
+	int nbrs; //nuber of nbrs
 
 public:
-	lattice(int N, int neighbours) : N(N), L(new int[N]), neighbours(neighbours) {
+	lattice(int N, int nbrs) : N(N), L(new int[N]), nbrs(nbrs) {
 		cout << "lattice(" << N << ")" << endl;
 	}
 
@@ -25,8 +25,8 @@ public:
 			L[i] = 2 * (rand() % 2) - 1;
 	}
 
-	virtual int sum_neighbours(int index) {//returns sum of neighbour spins
-		return 0;
+	virtual int sum_nbr(int index) {//returns sum of neighbour spins
+		return index = 0;
 	}
 
 	virtual void show() {
@@ -48,7 +48,7 @@ public:
 		cout << "sq_lattice(" << A << "*" << B << ")" << endl;
 	}
 
-	int sum_neighbours(int index) {
+	int sum_nbr(int index) {
 		int a = index / B, b = index % B;
 		return L[B * ((a + A - 1) % A) + b] +
 		+ L[B * ((a + 1) % A) + b] +
@@ -76,7 +76,7 @@ public:
 class parameters {
 protected:
 	int steps; //number of steps in simulation
-	double T; //temperature in Kelvins
+	//double T; //temperature in Kelvins - needs later
 	double beta; //beta = 1/kT
 	double J; //constant of material
 	double h; //outer magnet field
@@ -91,20 +91,20 @@ class Monte_Carlo : public parameters {
 	double *prob_arr; //array of all possible probabilities
 
 public:
-	Monte_Carlo (parameters p, lattice *l) : parameters(p), l(l), prob_arr(new double [1 + l->neighbours]) {
-		for (int i = 0; i <= l->neighbours; i++)
-			prob_arr[i] =  1 / (1 + exp(-2 * (2 * i - l->neighbours) * beta));
+	Monte_Carlo (parameters p, lattice *l) : parameters(p), l(l), prob_arr(new double [1 + l->nbrs]) {
+		for (int i = 0; i <= l->nbrs; i++)
+			prob_arr[i] =  1 / (1 + exp(-2 * (2 * i - l->nbrs) * beta));
 		cout << "Monte_Carlo()" << endl;
 	}
 
 	void simulate()	{
 		for (int i = 0; i < steps; i++) {
 			for (int j = 0; j < l->N; j++) {
-				int X = rand_30bit() % (l->N);
-				if (((double) rand() / RAND_MAX) < prob_arr[(l->neighbours + l->sum_neighbours(X)) / 2])
-					l->L[X] = 1;
+				int rand_spin = big_rand() % l->N;
+				if (((double) rand() / RAND_MAX) < prob_arr[(l->nbrs + l->sum_nbr(rand_spin)) / 2])
+					l->L[rand_spin] = 1;
 				else
-					l->L[X] = -1;
+					l->L[rand_spin] = -1;
 			}
 		}
 	}
@@ -122,7 +122,6 @@ void Monte_Carlo::test() {//test here
 	l->fill_random();
 	cout << "step 0:" << endl;
 	l->show();
-
 	cout << "step " << steps << ":" << endl;
 	simulate();
 	l->show();
@@ -132,11 +131,10 @@ int main() {
 	parameters p(500, 0.44); //steps, beta
 	Monte_Carlo model(p, new square_lattice(64, 64));
 	model.test();
-
 	return 0;
 }
 
-int rand_30bit() {
+int big_rand() { //30-bit random number
 	int r = rand() & 0x7FFF;
 	r = (r << 15) | (rand() & 0x7FFF);
 	return r;
