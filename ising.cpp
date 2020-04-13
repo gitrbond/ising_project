af73b4f42e926ca131de4edd5d1f2fefb40d4774
Roma Bondar <bond2000r@mail.ru>
2020-04-13 06:13:29 +0300

    Now class Monte_Carlo is no more friend of lattice, and many other fixes

--- ising.cpp
+++ ising.cpp
@@ -6,11 +6,12 @@
 #include <math.h>
 #include <ctime>
 #include <assert.h>
+
+//#define DEBUG
+
 #ifdef DEBUG
 #include "debug.h" //debug functions
 #endif
-
-//#define DEBUG
 
 using namespace std;
 
@@ -26,23 +27,24 @@ protected:
 
 public:
 	lattice(int N, int nbrs) : N(N), L(new int[N]), nbrs(nbrs) {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "lattice(" << N << ")" << endl;
-		#endif
+#endif
+		assert(nbrs > 0);
 	}
 
 	lattice(const lattice &old) : N(old.N), L(new int[N]), nbrs(old.nbrs) {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "lattice(" << N << ") copy constructor" << endl;
-		#endif
+#endif
 		for (int i = 0; i < N; i++)
 			L[i] = old.L[i];
 	}
 
 	lattice& operator = (const lattice &obj) {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "opertator = (const lattice &obj) assignment" << endl;
-		#endif
+#endif
 		N = obj.N;
 		L = new int[N];
 		nbrs = obj.nbrs;
@@ -51,13 +53,25 @@ public:
 		return *this;
 	}
 
+	int getN() {
+		return N;
+	}
+
+	int* getL() {
+		return L;
+	}
+
+	int getnbrs() {
+		return nbrs;
+	}
+
 	void fill_random() {
-		//srand(time(0));
 		for (int i = 0; i < N; i++)
 			L[i] = 2 * (rand() % 2) - 1;
 	}
 
 	int sum_nbr(int index) const { //returns sum of neighbour spins
+		assert(nbrs);
 		int *nbr_arr = new int[nbrs];
 		get_nbrs(index, nbr_arr);
 		int sum = L[nbr_arr[0]];
@@ -78,13 +92,13 @@ public:
 	}
 
 	virtual ~lattice() {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "~lattice()" << endl;
-		#endif
+#endif
 		delete [] L;
 	}
 
-	friend class Monte_Carlo;
+	//friend class Monte_Carlo;
 };
 
 class square_lattice : public lattice {
@@ -92,13 +106,14 @@ class square_lattice : public lattice {
 
 public:
 	square_lattice(int A, int B) : lattice(A * B, 4), A(A), B(B) {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "sq_lattice(" << A << "*" << B << ")" << endl;
-		#endif
+#endif
+		assert(A > 0 && B > 0);
 	}
 
 	void get_nbrs(int index, int *arr) const { //returns array of nbr indexes [U, D, L, R]
-		assert (B);
+		assert(nbrs >= 4);
 		int a = index / B, b = index % B;
 		arr[0] = B * ((a + A - 1) % A) + b;
 		arr[1] = B * ((a + 1) % A) + b;
@@ -108,20 +123,16 @@ public:
 
 	void show() const {
 		for (int i = 0; i < A; i++) {
-			for (int j = 0; j < B; j++)	{
-				if (L[B * i + j] > 0)
-					cout << "+"; //+1
-				else
-					cout << "."; //-1
-			}
+			for (int j = 0; j < B; j++)
+				cout << (L[B * i + j] > 0 ? "+" : ".");
 			cout << endl;
 		}
 	}
 
 	~square_lattice() {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "~sq_lattice()" << endl;
-		#endif
+#endif
 	}
 };
 
@@ -135,67 +146,67 @@ protected:
 
 public:
 	parameters(double beta, double H = 0, double J = 1, double mu = 1) : beta(beta), H(H), J(J), mu(mu) {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "parameters()" << endl;
-		#endif
+#endif
 	}
 
 	virtual ~parameters() {
-		#ifdef DEBUG
+#ifdef DEBUG
 		cout << "~parameters()" << endl;
-		#endif
+#endif
 	}
 };
 
 class Monte_Carlo : public parameters {
-	lattice *l;
-
 public:
-	Monte_Carlo (parameters &p, lattice *l) : parameters(p), l(l) {
-		#ifdef DEBUG
+	Monte_Carlo (parameters &p) : parameters(p) {
+#ifdef DEBUG
 		cout << "Monte_Carlo()" << endl;
-		#endif
+#endif
 	}
 
-	void simulate(int steps) const {
-		int *prob_arr = new int [1 + l->nbrs]; //array of all possible probabilities
-		for (int i = 0; i <= l->nbrs; i++)
-			prob_arr[i] =  round(RAND_MAX / (1 + exp(-2 * beta *((2 * i - l->nbrs) + mu * H))));
+	void simulate(lattice *l, int steps) const {
+		int nbrs = l->getnbrs();
+		int *prob_arr = new int [1 + l->getnbrs()]; //array of all possible probabilities
+		for (int i = 0; i <= nbrs; i++)
+			prob_arr[i] =  round(RAND_MAX / (1 + exp(-2 * beta *((2 * i - nbrs) + mu * H))));
 		for (int i = 0; i < steps; i++) {
-			for (int j = 0; j < l->N; j++) {
-				int rand_spin = big_rand() % l->N;
-				int prob = prob_arr[(l->nbrs + l->sum_nbr(rand_spin)) / 2];
-				l->L[rand_spin] = def_spin(prob);
+			for (int j = 0; j < l->getN(); j++) {
+				int rand_spin = big_rand() % l->getN();
+				assert((nbrs + l->sum_nbr(rand_spin)) / 2 < 1 + nbrs);
+				int prob = prob_arr[(nbrs + l->sum_nbr(rand_spin)) / 2];
+				l->getL()[rand_spin] = def_spin(prob);
 			}
 		}
 		delete [] prob_arr;
 	}
 
-	void clasters_simulate() const {
-		int steps = 10 * sqrt(l->N) * exp(-3 * beta);
+	void clasters_simulate(lattice *l) const {
+		int steps = 10 * sqrt(l->getN()) * exp(-3 * beta);
 		int prob = RAND_MAX * (1 - exp(-2 * beta)); //magical number
-		int *nbr_arr = new int[l->nbrs];
+		int *nbr_arr = new int[l->getnbrs()];
 		for (int j = 0; j < steps; j++) {
-			int spin = big_rand() % l->N;
+			int spin = big_rand() % l->getN();
 			vector <int> Claster {spin}, Pocket {spin};
 			while (Pocket.size()) {
 				spin = Pocket[big_rand() % Pocket.size()]; //randomly choose from pocket
 				l->get_nbrs(spin, nbr_arr);
-				for (int i = 0; i < l->nbrs; i++) {
+				for (int i = 0; i < l->getnbrs(); i++) {
 					if (rand() < prob && //take spin with probability
-						l->L[spin] == l->L[nbr_arr[i]] && !vcontains(Claster, nbr_arr[i])) {
+						l->getL()[spin] == l->getL()[nbr_arr[i]] && !vcontains(Claster, nbr_arr[i])) {
 						Pocket.push_back(nbr_arr[i]); //add it to pocket
 						Claster.push_back(nbr_arr[i]); //and to claster
 					}
 				}
 				assert(vdel(Pocket, spin)); //delete from pocket
-				#ifdef DEBUG
+#ifdef DEBUG
 				Dshow("Pocket", Pocket);
 				Dshow("Claster", Claster);
-				#endif
+#endif
 			}
-			for (auto i = Claster.begin(), end = Claster.end(); i != end; i++)
-				l->L[*i] = -l->L[*i]; //flipping claster
+			for (auto i = Claster.begin(), end = Claster.end(); i != end; ++i)
+				l->getL()[*i] = -l->getL()[*i]; //flipping claster
 		}
 		delete [] nbr_arr;
 	}
@@ -203,41 +214,34 @@ public:
 	int def_spin(int plus_prob) const {
 		int rand_prob = rand();
 		if (rand_prob < plus_prob)
-			return +1;
+			return 1;
 		return -1;
 	}
 
-	void test();
-
-	~Monte_Carlo() {
-		#ifdef DEBUG
-		cout << "~Monte_Carlo()" << endl;
-		#endif
-		delete l;
-	}
+	void test(lattice *l);
 };
 
-void Monte_Carlo::test() {//test here
+void Monte_Carlo::test(lattice *l) {//test here
 	l->fill_random();
 	cout << "step 0:" << endl;
 	l->show();
 	cout << "avg. magn = " << l->avg_magn() << endl;
 
-	int steps = 300;
-	//simulate(steps);
-	clasters_simulate();
+	int steps = 1;
+	simulate(l, steps);
+	clasters_simulate(l);
 	cout << "step " << steps << ":" << endl;
 	l->show();
 	cout << "avg. magn = " << l->avg_magn() << endl;
-	//lattice l1 = *l;
-	//l1.show();
 }
 
 int main() {
-	parameters p(0.9); //beta, H
+	//srand((unsigned)time(NULL));
+	parameters p(0.9); //beta
 	square_lattice *l = new square_lattice(4, 4);
-	Monte_Carlo model(p, l);
-	model.test();
+	Monte_Carlo model(p);
+	model.test(l);
+	delete l;
 	return 0;
 }
 
