00901c018ea8b35c1243657526dfe53045198f75
Stanislav <stanislav.pyatkin@mail.ru>
2020-05-09 19:26:13 +0300

    add feature: write data into file

--- ising.cpp
+++ ising.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector> //essential
 #include <stdlib.h>
 #include <algorithm>
@@ -15,9 +16,9 @@
 
 using namespace std;
 
-inline bool vcontains(const vector <int> &v, int el);
-bool vdel(vector <int> &v, int el);
-void vlinfill(vector <double> &v, const double start, const double end, const int points);
+inline bool vcontains(const vector < int > &v, int el);
+bool vdel(vector < int > &v, int el);
+void vlinfill(vector < double > &v, const double start, const double end, const int points);
 int big_rand();
 
 class lattice { //abstract
@@ -124,7 +125,7 @@ public:
 };
 
 class square_lattice : public rect_lattice {
-const int A; //A x A lattice
+	const int A; //A x A lattice
 
 public:
 	square_lattice(int A) : rect_lattice(A, A), A(A) {
@@ -195,7 +196,13 @@ public:
 		int *nbr_arr = new int[l->getnbrs()];
 		for (int j = 0; j < steps; j++) {
 			int spin = big_rand() % l->getN();
-			vector <int> Claster {spin}, Pocket {spin};
+			vector <int> Claster {
+				spin
+			}
+			, Pocket {
+				spin
+			}
+			;
 			while (Pocket.size()) {
 				spin = Pocket[big_rand() % Pocket.size()]; //randomly choose from pocket
 				l->get_nbrs(spin, nbr_arr);
@@ -225,45 +232,80 @@ public:
 		return -1;
 	}
 
-	void plot_magn_beta(lattice *l, const vector <double> &beta_points, vector <double> &magn_points, const int steps, const int averaging) { //the 5th version
+	void plot_magn_beta(lattice *l, const vector < double > &beta_points, vector < double > &magn_points, const int steps, const int averaging) { //the 5th version
 		assert(averaging > 0);
 		magn_points.clear();
+		
+		const char *filename = "data_magn_beta";
+		ofstream output(filename);
+		if (output) {
+			//output << parametrs;
 
-		for (auto i = beta_points.begin(); i != beta_points.end(); ++i) {
-			beta = *i;
-			double avg_magn = 0;
+			for (auto i = beta_points.begin(); i != beta_points.end(); ++i) {
+				beta = *i;
+				double avg_magn = 0;
 
-			for(int j = 0; j < averaging; j++) {
-				l->fill_random();
-				simulate(l, steps);
-				avg_magn += abs(l->avg_magn());
-				cout << ".";
+				for(int j = 0; j < averaging; j++) {
+					l->fill_random();
+					simulate(l, steps);
+					double mes;
+					mes = abs(l->avg_magn());
+					output << mes << " ";
+					avg_magn += mes;
+					cout << ".";
+				}
+				avg_magn /= averaging;
+				magn_points.push_back(avg_magn);
+				output << endl << beta << " " << magn_points.back() << endl;
+				cout << magn_points.back() << endl;
 			}
-			avg_magn /= averaging;
-			magn_points.push_back(avg_magn);
-			cout << magn_points.back() << endl;
-		}
+			output << endl;
+			
+			for (auto i = magn_points.begin(); i != magn_points.end(); ++i) {
+				output << *i << endl;
+			}
+
+		} else
+			cerr << "Unable to open output file " << filename << endl;
 	}
 
-	void plot_magn_H(lattice *l, const vector <double> &H_points, vector <double> &magn_points, const int plot_beta, const int steps, const int averaging) { //the 5th version
+	void plot_magn_H(lattice *l, const vector < double > &H_points, vector < double > &magn_points, const int plot_beta, const int steps, const int averaging) { //the 5th version
 		assert(averaging > 0);
 		magn_points.clear();
 		beta = plot_beta;
 
-		for (auto i = H_points.begin(); i != H_points.end(); ++i) {
-			H = *i;
-			double avg_magn = 0;
+		const char *filename = "data_magn_H";
+		ofstream output(filename);
+		if (output) {
+			//output << parametrs;
 
-			for(int j = 0; j < averaging; j++) {
-				l->fill_random();
-				simulate(l, steps);
-				avg_magn += l->avg_magn();
-				cout << ".";
+			for (auto i = H_points.begin(); i != H_points.end(); ++i) {
+				H = *i;
+				double avg_magn = 0;
+
+				for(int j = 0; j < averaging; j++) {
+					l->fill_random();
+					simulate(l, steps);
+					double mes;
+					mes = l->avg_magn();
+					output << mes << " ";
+					avg_magn += mes;
+					cout << ".";
+				}
+				avg_magn /= averaging;
+				magn_points.push_back(avg_magn);
+				output << endl << beta << " " << magn_points.back() << endl;
+				cout << magn_points.back() << endl;
 			}
-			avg_magn /= averaging;
-			magn_points.push_back(avg_magn);
-			cout << magn_points.back() << endl;
-		}
+			output << endl;
+
+			for (auto i = magn_points.begin(); i != magn_points.end(); ++i) {
+				output << *i << endl;
+			}
+
+		} else
+			cerr << "Unable to open output file " << filename << endl;
+
 	}
 
 	void test(lattice *l);
@@ -300,19 +342,19 @@ int main() {
 	vlinfill(x_points, -0.01, 0.01, 7); //fills vector with 11 values from 0 to 1
 	Dshow("H values (x)", x_points);
 
-	//model.plot_magn_beta(l, x_points, y_points, 400, 5); // ... steps, averaging
-	model.plot_magn_H(l, x_points, y_points, 0.8, 500, 1); // ... beta, steps, averaging
+	model.plot_magn_beta(l, x_points, y_points, 50, 5); // ... steps, averaging
+	model.plot_magn_H(l, x_points, y_points, 0.8, 50, 1); // ... beta, steps, averaging
 	Dshow("avg_magn values (y)", y_points);
 
 	delete l;
 	return 0;
 }
 
-inline bool vcontains(const vector <int> &v, int el) { //checks if vector contains element
-    return find(v.begin(), v.end(), el) != v.end();
+inline bool vcontains(const vector < int > &v, int el) { //checks if vector contains element
+	return find(v.begin(), v.end(), el) != v.end();
 }
 
-bool vdel(vector <int> &v, int el) { //deletes element from vector. returns false if not found
+bool vdel(vector < int > &v, int el) { //deletes element from vector. returns false if not found
 	auto del = find(v.begin(), v.end(), el);
 	if (del != v.end()) {
 		v.erase(del);
@@ -321,7 +363,7 @@ bool vdel(vector <int> &v, int el) { //deletes element from vector. returns fals
 	return false;
 }
 
-void vlinfill(vector <double> &v, const double start, const double end, const int points) {
+void vlinfill(vector < double > &v, const double start, const double end, const int points) {
 	for (int i = 0; i < points; i++)
 		v.push_back(start + i * (end - start) / (points - 1));
 }
